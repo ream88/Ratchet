53bd4c282865536e86edb2c04528e6a6201d6505
Mario Uher <uher.mario@gmail.com>
2026-06-01 11:16:58 +0200

    Initial Commit

---
+++ Ratchet/RatchetApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatchetApp.swift
+//  Ratchet
+//
+//  Created by Mario Uher on 01.06.26.
+//
+
+import SwiftUI
+
+@main
+struct RatchetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RatchetTests/RatchetTests.swift
@@ -0,0 +1,17 @@
+//
+//  RatchetTests.swift
+//  RatchetTests
+//
+//  Created by Mario Uher on 01.06.26.
+//
+
+import Testing
+@testable import Ratchet
+
+struct RatchetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
